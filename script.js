5cb9d1265ecf13ebb7aad124f1c50e6df6267b4c
Marcel <marwozniak01@gmail.com>
2025-08-19 19:01:56 +0200

    Dodałem możliwość wyświetlenia okna limitu znaków po zaznaczeniu odpowiedniego checkboxa

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const text = document.getElementById('textarea')
 let totalCharactersPlaceholder = document.getElementById('total-chars')
 let excludeSpacesBtn = document.getElementById('exclude-spaces')
+let setLimitBtn = document.getElementById('set-limit')
+const limitNumberBox = document.getElementById('limit-number')
 
 text.addEventListener('input', charCounting)
 
@@ -12,4 +14,13 @@ function charCounting() {
     : text.value;
 
   totalCharactersPlaceholder.textContent = cleanedText.length;
+}
+
+setLimitBtn.addEventListener('change', setLimit)
+
+function setLimit() {
+    limitNumberBox.style.display =
+        (limitNumberBox.style.display === "block")
+         ? "none"
+         : "block";
 }
